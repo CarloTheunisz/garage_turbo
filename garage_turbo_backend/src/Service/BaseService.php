1d4d8687d7c0452e8b601766c0c052065ff711c9
CarloTheunisz <carlo_theunisz1@hotmail.com>
2019-09-02 14:37:43 +0200

    Create base service

---
+++ garage_turbo_backend/src/Service/BaseService.php
@@ -0,0 +1,34 @@
+<?php
+namespace App\Service;
+use Doctrine\ORM\EntityManagerInterface;
+
+class BaseService {
+    private $rep;
+    private $em;
+
+    /**
+     * Functie: __construct
+     * Doel:    Constructor van de klasse
+     * Taken:   Verkrijgt de entity manager en repository
+     */
+    public function __construct($em, $entity) {
+        $this->em = $em;
+        $this->rep = $this->em->getRepository($entity);
+    }
+
+    /**
+     * Functie: find
+     * Doel:    vind de record met het meegegeven id
+     */
+    public function find($id) {
+        return $this->rep->find($id);
+    }
+
+    /**
+     * Functie: findAll
+     * Doel:    vind alle records van de entity
+     */
+    public function findAll() {
+        return $this->rep->findAll();
+    } 
+}
