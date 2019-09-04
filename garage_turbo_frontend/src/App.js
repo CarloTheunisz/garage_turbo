ddf40eb190cc42fe9c4d89c6804c8929edcb97a3
CarloTheunisz <carlo_theunisz1@hotmail.com>
2019-09-04 16:14:33 +0200

    Update kilometerstand every five seconds

--- garage_turbo_frontend/src/App.js
+++ garage_turbo_frontend/src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import API from './Library/API';
 import Header from './Components/Header';
 import Startpagina from './Components/Startpagina';
 import Genereer from './Components/Genereer';
 import Overzicht from './Components/Overzicht'
 
 function App() {
+  setInterval(() => {
+    API.fetchData('http://localhost/garage_turbo/garage_turbo_backend/public/api/update/kilometerstand/all', 'PUT')
+  }, 5000);
+
   return (
     <BrowserRouter>
       <Header/>
