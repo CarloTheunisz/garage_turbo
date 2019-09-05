441f04ab340204c0a772326dd4a80ecbe2d4483e
CarloTheunisz <carlo_theunisz1@hotmail.com>
2019-09-05 10:39:11 +0200

    See Overzicht after generating testdata

--- garage_turbo_frontend/src/Components/Genereer.js
+++ garage_turbo_frontend/src/Components/Genereer.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import API from '../Library/API';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './Header';
+import Overzicht from './Overzicht';
 
 class Genereer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            text: ""
+            hasGenerated: false
         }
     }
 
@@ -20,33 +22,38 @@ class Genereer extends Component {
             console.log(error)
         });
         this.setState({
-            text: 'Testdata gegenereerd. Bekijk het overzicht van klanten om de wijzigingen te zien.'
+            hasGenerated: true
         })
     }
 
     render() {
-        return (
-            <React.Fragment>
-                <Header path={ this.props.location.pathname }/>
-                <div className="normal-page">
-                    <div className="space-below">
-                        <b>Genereer Test Data</b>
+        if(this.state.hasGenerated) {
+            return (
+                <BrowserRouter>
+                    <Route exact path='/genereer' component={ Overzicht }/>
+                </BrowserRouter>
+            )
+        } else {
+            return (
+                <React.Fragment>
+                    <Header path={ this.props.location.pathname }/>
+                    <div className="normal-page">
+                        <div className="space-below">
+                            <b>Genereer Test Data</b>
+                        </div>
+                        <div>
+                            Er worden willekeurig tussen 10 en 100 klanten toegevoegd aan de database.
+                        </div>
+                        <div className="space-below">
+                            Bestaande gegevens worden verwijderd!
+                        </div>
+                        <button type="button" className="btn btn-primary btn-body space-below" onClick={ this.generate.bind(this) }>
+                            Genereer
+                        </button>
                     </div>
-                    <div>
-                        Er worden willekeurig tussen 10 en 100 klanten toegevoegd aan de database.
-                    </div>
-                    <div className="space-below">
-                        Bestaande gegevens worden verwijderd!
-                    </div>
-                    <button type="button" className="btn btn-primary btn-body space-below" onClick={ this.generate.bind(this) }>
-                        Genereer
-                    </button>
-                    <div>
-                        { this.state.text }
-                    </div>
-                </div>
-            </React.Fragment>
-        );
+                </React.Fragment>
+            );
+        }
     }
 }
  
