1165808e833c3e2c03dde9df4c36b601face7cbc
CarloTheunisz <carlo_theunisz1@hotmail.com>
2019-09-06 09:00:27 +0200

    Show new onderhoudsbeurten immediately

--- garage_turbo_frontend/src/Components/TabellenRij.js
+++ garage_turbo_frontend/src/Components/TabellenRij.js
@@ -3,7 +3,18 @@ import config from './../Config/Config';
 import API from './../Library/API'
 
 class Tabellenrij extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            beurtenAantal: this.props.data.beurtenAantal
+        }
+    }
+
     onClick(event) {
+        this.setState ({
+            beurtenAantal: this.state.beurtenAantal + 1
+        });
+
         var id = event.target.name;
         API.fetchData(config.baseUrl + config.performOnderhoud + id, 'PUT')
         .catch( error => {
@@ -18,7 +29,7 @@ class Tabellenrij extends Component {
                 <td>{ this.props.data.kenteken }</td>
                 <td>{ this.props.data.automerk.merknaam }</td>
                 <td>{ this.props.data.kilometerstand }</td>
-                <td>{ this.props.data.beurtenAantal }</td>
+                <td>{ this.state.beurtenAantal }</td>
                 <td><button className="btn btn-primary btn-body btn-table" name={ this.props.data.id } onClick={ this.onClick.bind(this) }>Voer uit</button></td>
             </tr>
         );
